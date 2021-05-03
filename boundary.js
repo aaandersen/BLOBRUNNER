c56fc52cfa89337b457cdcd5856ab8552f28d054
Anders <81511709+aaandersen@users.noreply.github.com>
2021-05-03 19:49:17 +0200

    Update boundary.js

--- boundary.js
+++ boundary.js
@@ -4,9 +4,12 @@ class Boundary {
         this.right = right
         this.bottom = bottom
         this.left = left
+        // this.dimension angiver tykkelse
         this.dimension = 10
     }
     show() {
+        // Rect(x-koordinatet til rektanglet, y-koordinatet til rektanglet, width/bredden, height/højden)
+
         fill(0)
         // Top
         rect(this.left, this.top, Math.abs(this.left) + this.right, this.dimension)
