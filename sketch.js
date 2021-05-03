aa4cc137914750812c7dae5ebb58712388447d44
Anders <81511709+aaandersen@users.noreply.github.com>
2021-05-03 19:50:33 +0200

    tilføjer kommentarer til koden

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+// Spil parametre
 const NUMBER_OF_EATABLES = 100
 const NUMBER_OF_POISONOUS = 150
 const NUMBER_OF_DEADLY = 0
@@ -7,33 +8,36 @@ const PLAYER_INITIAL_RADIUS = 40
 const BLOB_RADIUS = 15
 const BLOB_DIAMETER = BLOB_RADIUS * 2
 
+// Allokerer arrayet til blobsne
 let blobs = []
 let player
 let boundary
 let zoom = 1
-//implemntere tid til spillet.
+
+// Implemntere tid til spillet.
 let time = 0;
 let interval = setInterval(() => time++, 1000);
-// beskriver player størrelse
-let score = 7;
-//beskriver hvor mange grønne man har spist
-let scoregreen = 0;
-//soundtrack
-let music;
-//lyd der spiller når man dør
-let GameOver
-//lyd til skade
-let Hit
-//lyd til point
-let Point
-//lyd til win 
-let Win
 
+// Beskriver player størrelse
+let score = 7;
+
+// Varialbe der tjekker antal BlobEatable spist
+let scoregreen = 0;
+
+// Soundtrack og lydeffekter
+let music;
+let Point
+let Hit 
+let Win
+let GameOver
+
+// Knap
 let resetButton
 
+// Sætter gameState = null 
 let gameState = null
 
-let col = color(25, 23, 200, 50);
+// Preload() loader musikken ind, inden setup kører 
 function preload() {
     // load either beatbox.mp3, or .ogg, depending on browser
     music = loadSound("assets/Soundtrack.mp3");
@@ -44,22 +48,26 @@ function preload() {
 }
 
 function setup() {
+    // HSB farvespectrum, Hvilket erstatter RGB. 
     colorMode(HSB, 100)
     createCanvas(windowWidth, windowHeight)
 
+    // Knap logik
     resetButton = createButton('RESET?')
     resetButton.position(windowWidth*0.5, windowHeight*0.6);
     resetButton.mousePressed(initialize)
     resetButton.style('font-size', '50px');
 
-    // boundary
+    // Boundary
     boundary = new Boundary(-windowHeight, windowWidth, windowHeight, -windowWidth)
 
+    // Kalder initialize()
     initialize()
 }
 
-function initialize() {
 
+function initialize() {
+    // Opstart parametre
     player = new BlobPlayer(0, 0, PLAYER_INITIAL_RADIUS)
     blobs = []
     zoom = 1
@@ -70,61 +78,71 @@ function initialize() {
     GameOver.stop();
     Win.stop();
     music.stop();
+    // Starter Spillets soundtrack
     music.play();
 
+    // Gemmer reset button, så den ikke på skærmen under gameState = 'playing'
     resetButton.hide()
 
-    // BlobEatable
+    // BlobEatable spawn
     for (var i = 0; i < NUMBER_OF_EATABLES; i++) {
         spawnBlob(BlobEatable)
     }
 
-    // BlobPoisonous
+    // BlobPoisonous spawn
     for (var i = 0; i < NUMBER_OF_POISONOUS; i++) {
-       spawnBlob(BlobPoisonous)
+        spawnBlob(BlobPoisonous)
     }
 
-    // BlobDeadly
+    // BlobDeadly spawn
     for (var i = 0; i < NUMBER_OF_DEADLY; i++) {
         spawnBlob(BlobDeadly)
     }
 
-    //Sætter gameState til playing
+    // Sætter gameState til playing
     gameState = 'playing'
   
-
+    // sætter initialize() til at loope
     loop()
 }
 
 
 function draw() {
     // B A C K G R O U N D
-    background('grey');
+    background(255);
 
     push()
     // L O G I C
+    // player.radius angiver spillets zoom, jo størrere radius, jo mindre zoom.
     const newzoom = 64 / player.radius
+    // Lerp anvendes til, at give en glidende overgang fra zoom til newzoom
     zoom = lerp(zoom, newzoom, 0.1)
+    // Her translates canvasset, så blobplayer er i midten
     translate(windowWidth * 0.5, windowHeight * 0.5)
+    // Scale anvendes til at bevare blobplayers  størrelses på skærmen, så blobplayer bliver samme størrelse samt med, at verden omkring bliver mindre
+    // scale(.45) // @DEBUG
     scale(zoom)
+    // Her translates canvasset mod blobplayers retningsvektor,så blobplayers forbliver er i midten, under bevægelse
     translate(-player.pos.x, -player.pos.y)
-    //scale(.45) // @DEBUG
+    
     
 
-    //undersøg hvad en arrow funktion og hvad en filter funktion i javascript er, få styr på det.
+    
+    // Deklarerer længden af blobsne, som en const.
     const currentBlobEastablesLength = blobs.filter(blob => blob.constructor.name === 'BlobEatable').length
     const currentBlobPoisonousLength = blobs.filter(blob => blob.constructor.name === 'BlobPoisonous').length
     const currentBlobDeadlyLength = blobs.filter(blob => blob.constructor.name === 'BlobDeadly').length
 
-    //spawn Deadly
+    
     //console.log(currentBlobEastablesLength, SPAWN_DEADLY_BLOBS_EATABLE_LENGTH)
+    // Spawn BlobDeadly, ved et given antal BlobEastable spist logik
     if (currentBlobEastablesLength < SPAWN_DEADLY_BLOBS_EATABLE_LENGTH && currentBlobDeadlyLength === 0) {
         for (let index = 0; index < DEADLY_SPAWNCOUNT; index++) {
             spawnBlob(BlobDeadly)
         }
     }
 
-    
+    // Tjekkes blandt alle blobs om de er spist, hvis de er spist, fjernes de fra arrayet og der spawnes en BlobPoisonous
     blobs.forEach((blob, idx) => {
         blob.update()
         blob.show()
@@ -134,27 +152,33 @@ function draw() {
         }
     })
 
+    // Tjekker om der er kontakt mellem Blobplayer og Boundary 
     player.update(boundary)
     player.show()
 
+    
     boundary.show()
     
     pop()
 
-    ///kører hele vores UI i draw
+    // Kører hele vores UI i draw
     UI()
 
 }
 
+// Funktionen der anvendes til at skabe vores blobs
 function spawnBlob(constructor,
     x = random(-windowWidth + BLOB_DIAMETER, windowWidth - BLOB_DIAMETER),
     y = random(-windowHeight + BLOB_DIAMETER, windowHeight - BLOB_DIAMETER),
     radius = BLOB_DIAMETER) {
-
+    
+    // Tilføjer vores nye blob til enden af arrayet med de eksisterende blobs
     blobs.push(new constructor(x, y, BLOB_RADIUS))
 }
 
+// Logikken bare vores UI
 function UI() {
+    // Tjekker hvilket gamestate player har opnået
     if (gameState === 'playing') {
         if (score <= 1) {
             gameState = 'gameover'
@@ -163,18 +187,22 @@ function UI() {
         }
     }
 
+    // Udfører 
     if (gameState === 'playing') {
         UIPlaying()
     } else if (gameState === 'gameover') {
         UIGameOver()
         music.stop();
         GameOver.play();
+        // Lader os se, "reset" buttion
         resetButton.show()
+        // Forhindre draw loope koden, når gameState = 'gameover'
         noLoop()
     } else if (gameState === 'win') {
         UIWin()
         music.stop();
         Win.play();
+        // Rykker "reset" buttion, så den paser ind på 'win' skærmen
         resetButton.position(windowWidth*0.25, windowHeight*0.7);
         resetButton.show()
         noLoop()
@@ -183,23 +211,24 @@ function UI() {
 }
 
 function UIPlaying() {
-    //push() pop() anvendes til at inkapsle kode som textfonts, fill osv til et bestemt område.
+        // push() pop() anvendes til at inkapsle kode som textfonts, fill osv til et bestemt område.
     push()
-            //printer tiden gået i sekunder
+        // Sætter tekstfonten til en mere interrasant "monospace" font
         textFont("monospace", 60);
         fill('black')
+        // Printer tiden gået i sekunder
         text(time, windowWidth / 2, 10, 100, 100);
         pop()
 
         push()
-        // Printer Players størrelse nummerisk
         textFont("monospace", 30);
         fill('black')
+         // Printer Players størrelse nummerisk
         text(score, 10, 10, 80, 80);
         pop()
 
         push()
-        //printer vores samlede antal spiste grønne blobs i grøn
+        // Printer vores samlede antal spiste grønne blobs i grøn
         fill('green')
         noStroke();
         textFont("monospace", 30);
@@ -209,19 +238,19 @@ function UIPlaying() {
 
 function UIGameOver() {
     push()
+
         textFont("monospace", 150);
+        // Sætter baggrounden til rød
         background('red');
-        
         // Konstanter der ganges på windowWidth og windowHeight passer med, at teskt rykkes til midten.
         text('GAME OVER', windowWidth * 0.25, windowHeight * 0.4, windowWidth, windowHeight);
         textSize(50);
         fill('green');
         noStroke();
-        // Samme som overstående tekst, gælder på følgende.
         text('Du fik ' + scoregreen + ' points', windowWidth * 0.25, windowHeight * 0.6, windowWidth, windowHeight);
         // Rykker blobplayers position væk fra boundary
         player.x = windowWidth * 3;
-        // Følger overstående, bare hvor der tjekkes om alle grønne blobs er spist. og vinder skærm istedet.
+        
     pop()
 
 }
@@ -229,16 +258,17 @@ function UIGameOver() {
 function UIWin() {
     push()
         textFont("monospace", 150);
+        // Sætter baggrounden til gul
         background('yellow');
 
-        // Konstanter der ganges på windowWidth og windowHeight passer med, at teskt rykkes til midten.
         text('Winner', windowWidth * 0.25, windowHeight * 0.4, windowWidth, windowHeight);
         textFont("monospace", 60);
-        // Konstanter der ganges på windowWidth og windowHeight passer med, at teskt rykkes til midten.
+
         text('Du vandt på ' + time + ' sekunder', windowWidth * 0.25, windowHeight * 0.6, windowWidth, windowHeight);
 
+        // Stopper spillets timner
         clearInterval(interval)
-        // Rykker blobplayers position væk fra boundary
+       
         player.x = windowWidth * 3;
     pop()
 }
